d11bf8be4612ef8ad5c2c9c9a3288f2725ae594b
giovanacanalli-escola <giovana.canalli@escola.pr.gov.br>
2026-08-26 12:17:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const imagem = document.getElementsByTagName("img")
+
+addEventListener("click", function ({
+
+    imagem.src = "https://preview.redd.it/was-anyone-else-not-really-upset-about-erens-death-v0-mp4g9iufo7xf1.jpeg?width=640&crop=smart&auto=webp&s=e925e17ef1ca6858711b6fbfecf411a0019e2108"
+
+}))
